2a9d70e28c635416be66cb78f643da76fa1ab4bd
Todd-J-Topicz <tjtopicz@gmail.com>
2023-05-03 15:39:59 -0500

    Made altercations to table creations on migration.sql

--- api/src/db/migration.sql
+++ api/src/db/migration.sql
@@ -6,6 +6,23 @@ DROP TABLE IF EXISTS branch_tasks;
 DROP TABLE IF EXISTS tasks;
 DROP TABLE IF EXISTS branch;
 
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE branch (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (50)
+);
+
+CREATE TABLE branch_tasks (
+  id SERIAL PRIMARY KEY,
+  task_id INT NOT NULL,
+  branch_id INT NOT NULL,
+  FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
+  FOREIGN KEY (branch_id) REFERENCES branch(id) ON DELETE CASCADE
+);
 
 CREATE TABLE instructors (
   id SERIAL PRIMARY KEY,
@@ -13,9 +30,7 @@ CREATE TABLE instructors (
   middlename VARCHAR (30),
   lastname VARCHAR (30) NOT NULL,
   email VARCHAR (255) NOT NULL,
-  location VARCHAR (50) NOT NULL, 
-  cohort_id INT NOT NULL,
-  FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE
+  location VARCHAR (50) NOT NULL
 );
 
 
@@ -27,48 +42,41 @@ CREATE TABLE students (
   age INT NOT NULL,
   location VARCHAR (50) NOT NULL,
   base VARCHAR (50) NOT NULL,
-  active BOOLEAN NOT NULL,
-  branch_id INT NOT NULL,
-  instructor_id INT NOT NULL,
-  cohort_id INT NOT NULL,
-  FOREIGN KEY (branch_id) REFERENCES branch(id) ON DELETE CASCADE,  
-  FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
-  FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE
+  active BOOLEAN NOT NULL
 );
 
 
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY,
-  courseid VARCHAR (255) NOT NULL,
-  instructor_id INT NOT NULL,
-  student_id INT NOT NULL,
-  FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
-  FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE
+  courseid VARCHAR (255) NOT NULL
 );
 
 CREATE TABLE student_tasks (
   id SERIAL PRIMARY KEY,
-  completed BOOLEAN NOT NULL,
-  student_id INT NOT NULL,
-  branch_tasks_id INT NOT NULL,
-  FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
-  FOREIGN KEY (branch_tasks_id) REFERENCES branch_tasks(id) ON DELETE CASCADE
+  completed BOOLEAN NOT NULL
 );
 
-CREATE TABLE branch_tasks (
-  id SERIAL PRIMARY KEY,
-  task_id INT NOT NULL,
-  branch_id INT NOT NULL,
-  FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE;
-  FOREIGN KEY (branch_id) REFERENCES branch(id) ON DELETE CASCADE
-);
 
-CREATE TABLE tasks (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL
-);
+ALTER TABLE instructors 
+ADD COLUMN cohort_id INT NOT NULL,
+ADD CONSTRAINT fk_cohort_id FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE;
 
-CREATE TABLE branch (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR (50)
-);
+ALTER TABLE students 
+ADD COLUMN branch_id INT NOT NULL,
+ADD COLUMN instructor_id INT NOT NULL,
+ADD COLUMN cohort_id INT NOT NULL,
+ADD CONSTRAINT fk_branch_id FOREIGN KEY (branch_id) REFERENCES branch(id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_cohort_id FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE;
+
+ALTER TABLE cohorts 
+ADD COLUMN instructor_id INT NOT NULL,
+ADD COLUMN student_id INT NOT NULL,
+ADD CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE;
+
+ALTER TABLE student_tasks
+ADD COLUMN student_id INT NOT NULL,
+ADD COLUMN branch_tasks_id INT NOT NULL,
+ADD CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
+ADD CONSTRAINT fk_branch_tasks_id FOREIGN KEY (branch_tasks_id) REFERENCES branch_tasks(id) ON DELETE CASCADE;
--- api/src/db/seed.sql
+++ api/src/db/seed.sql
@@ -1,5 +1,17 @@
-INSERT INTO tasks(description) VALUES('Do the dishes');
-INSERT INTO tasks(description) VALUES('Walk the dog');
-INSERT INTO tasks(description) VALUES('Sweep the floor');
-INSERT INTO tasks(description) VALUES('Do your homework');
-INSERT INTO tasks(description) VALUES('Beat Elden Ring');
+-- INSERT INTO tasks(description) VALUES('Do the dishes');
+-- INSERT INTO tasks(description) VALUES('Walk the dog');
+-- INSERT INTO tasks(description) VALUES('Sweep the floor');
+-- INSERT INTO tasks(description) VALUES('Do your homework');
+-- INSERT INTO tasks(description) VALUES('Beat Elden Ring');
+
+-- DROP TABLE IF EXISTS instructors;
+-- DROP TABLE IF EXISTS students;
+-- DROP TABLE IF EXISTS cohorts;
+-- DROP TABLE IF EXISTS student_tasks;
+-- DROP TABLE IF EXISTS branch_tasks;
+-- DROP TABLE IF EXISTS tasks;
+-- DROP TABLE IF EXISTS branch;
+
+INSERT INTO instructors('firstname', 'middlename', 'lastname', 'email', 'location', 'cohort_id') VALUES
+    ('Jane', 'Dunno', 'Doe', 'JaneDoe@fakemail.com', 1),
+    ('Jon', 'Doe')
