cafffe587f2ac9cd0d0a6a0bc0a0087b0a4d812c
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-05-03 16:08:01 -0500

    Initial login modal set up with context, not yet incorporated in app.jsx

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,1 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
+import axios from "axios";
+import useLogin from "../context/LoginContext"
+
+const LoginModal = () => {
+    const { showLogin, loginPosition, handleMouseClick } = useContext(LoginContext);
+    const isEmailValid = (email) => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
+}
+
+export default LoginModal;
