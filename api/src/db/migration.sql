b01befd77f9d5d32359111660f077f70268ff84e
Todd-J-Topicz <tjtopicz@gmail.com>
2023-05-03 17:21:13 -0500

    created fake seed data for seed.sql, fixed a few errors on migration.sql

--- api/src/db/migration.sql
+++ api/src/db/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE tasks (
 
 CREATE TABLE branch (
   id SERIAL PRIMARY KEY,
-  name VARCHAR (50)
+  name TEXT NOT NULL
 );
 
 CREATE TABLE branch_tasks (
@@ -30,7 +30,7 @@ CREATE TABLE instructors (
   middlename VARCHAR (30),
   lastname VARCHAR (30) NOT NULL,
   email VARCHAR (255) NOT NULL,
-  location VARCHAR (50) NOT NULL
+  location TEXT NOT NULL
 );
 
 
@@ -40,8 +40,8 @@ CREATE TABLE students (
   middlename VARCHAR (30),
   lastname VARCHAR (30) NOT NULL, 
   age INT NOT NULL,
-  location VARCHAR (50) NOT NULL,
-  base VARCHAR (50) NOT NULL,
+  location TEXT NOT NULL,
+  base TEXT,
   active BOOLEAN NOT NULL
 );
 
@@ -58,25 +58,25 @@ CREATE TABLE student_tasks (
 
 
 ALTER TABLE instructors 
-ADD COLUMN cohort_id INT NOT NULL,
+ADD COLUMN cohort_id INT,
 ADD CONSTRAINT fk_cohort_id FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE;
 
 ALTER TABLE students 
-ADD COLUMN branch_id INT NOT NULL,
-ADD COLUMN instructor_id INT NOT NULL,
-ADD COLUMN cohort_id INT NOT NULL,
+ADD COLUMN branch_id INT,
+ADD COLUMN instructor_id INT,
+ADD COLUMN cohort_id INT,
 ADD CONSTRAINT fk_branch_id FOREIGN KEY (branch_id) REFERENCES branch(id) ON DELETE CASCADE,
 ADD CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
 ADD CONSTRAINT fk_cohort_id FOREIGN KEY (cohort_id) REFERENCES cohorts(id) ON DELETE CASCADE;
 
 ALTER TABLE cohorts 
-ADD COLUMN instructor_id INT NOT NULL,
-ADD COLUMN student_id INT NOT NULL,
+ADD COLUMN instructor_id INT,
+ADD COLUMN student_id INT,
 ADD CONSTRAINT fk_instructor_id FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE,
 ADD CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE;
 
 ALTER TABLE student_tasks
-ADD COLUMN student_id INT NOT NULL,
-ADD COLUMN branch_tasks_id INT NOT NULL,
+ADD COLUMN student_id INT,
+ADD COLUMN branch_tasks_id INT,
 ADD CONSTRAINT fk_student_id FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
 ADD CONSTRAINT fk_branch_tasks_id FOREIGN KEY (branch_tasks_id) REFERENCES branch_tasks(id) ON DELETE CASCADE;
