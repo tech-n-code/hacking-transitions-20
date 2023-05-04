38c1372561eabf68c98458634c3c51e082bf0553
alphie1028 <alphie1028@gmail.com>
2023-05-03 21:34:42 -0400

    basic functionality of modals oppening/closing

--- client/src/components/AddEditModals/AddCohortModal.jsx
+++ client/src/components/AddEditModals/AddCohortModal.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import "../../styles/AddEditModals.css"
+import LeftColumnContext from "../../context/LeftColumnContext";
 
 function AddCohortModal(){
+    const {closeAddCohort} = useContext(LeftColumnContext)
     return(
-        <>
-        </>
+        <h1 onClick={closeAddCohort}>Hello World</h1>
     )
 }
 export default AddCohortModal;
--- client/src/components/AddEditModals/EditCohortModal.jsx
+++ client/src/components/AddEditModals/EditCohortModal.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import "../../styles/AddEditModals.css"
+import LeftColumnContext from "../../context/LeftColumnContext";
 
 function EditCohortModal(){
+    const {closeEditCohort} = useContext(LeftColumnContext)
     return(
-        <></>
+        <h1 onClick={closeEditCohort}>Hi</h1>
     )
 }
 export default EditCohortModal;
