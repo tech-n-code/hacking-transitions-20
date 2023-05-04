47c3c98314cd669d2714c19530783e7ac969349e
alphie1028 <alphie1028@gmail.com>
2023-05-03 22:20:28 -0400

    basic strcuture of modals

--- client/src/components/AddEditModals/EditCohortModal.jsx
+++ client/src/components/AddEditModals/EditCohortModal.jsx
@@ -5,7 +5,26 @@ import LeftColumnContext from "../../context/LeftColumnContext";
 function EditCohortModal(){
     const {closeEditCohort} = useContext(LeftColumnContext)
     return(
-        <h1 onClick={closeEditCohort}>Hi</h1>
+        <div className="addEditContainer">
+            <div className="addEditHeader">
+                <div className="addEditClose" onClick={closeEditCohort}>Close</div>
+            </div>
+            <div className="addEditContent">
+                <input placeholder="Cohort Name"></input>
+                <div className="dateRange">
+                    Start Date:
+                    <input type="date"></input>
+                    End Date:
+                    <input type="date"></input>
+                </div>
+                <input placeholder="Instructor Name"></input>
+                <input placeholder="Student"></input>
+            </div>
+            <div className="addEditFooter">
+                <div className="addEditOption">Submit</div>
+                <div className="addEditOption" onClick={closeEditCohort}>Cancel</div>
+            </div>
+        </div>
     )
 }
 export default EditCohortModal;
