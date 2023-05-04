2d6a0fbe7aab2cf97d71aeb104e85212f08f6ce2
Todd-J-Topicz <tjtopicz@gmail.com>
2023-05-04 15:50:24 -0500

    Added 2 columns to student table, added some seed data to students.

--- api/src/db/seed.sql
+++ api/src/db/seed.sql
@@ -17,11 +17,16 @@ INSERT INTO branch_tasks (task_id, branch_id) VALUES
 
 INSERT INTO instructors (firstname, middlename, lastname, email, location) VALUES
     ('Jane', 'Dunno', 'Doe', 'JaneDoe@fakemail.com', 'Austin, Texas'),
-    ('Jon', 'Idunno', 'Doe', 'JonDoe@fakeemail.com', 'Cincinnati, OH');
+    ('Jon', NULL, 'Doe', 'JonDoe@fakeemail.com', 'Cincinnati, OH');
 
-INSERT INTO students (firstname, middlename, lastname, age, location, base, active) VALUES
-    ('Sarah', 'Lynn', 'Marks', 35, 'Clarksville, KY', 'Ft. Campbell, KY', TRUE),
-    ('Chris', 'P', 'Bacon', 80, 'Villages, FL', NULL, FALSE);
+INSERT INTO students (firstname, middlename, lastname, age, location, base, active, phonenumber, email) VALUES
+    ('Sarah', 'Lynn', 'Marks', 35, 'Clarksville, KY', 'Ft. Campbell', TRUE, 2707981234, 'stoneage@aol.com'),
+    ('Chris', 'P', 'Bacon', 80, 'Villages, FL', NULL, FALSE, 7276437382, 'Bacon&Eggs@fakemail.com'),
+    ('Mike Ro', 'P', 'Ennis', 25, 'San Francisco, CA', 'Travis Air Force Base', TRUE, 4158299673, 'OverCompensate@fakemail.com'),
+    ('Jed', 'I', 'Knight', 38, 'Elkhart, IN', 'Grisson Air Reserve Base', TRUE, 9304734927, 'ForceBeWithYou@fakemail.com'),
+    ('Deja', NULL, 'Viau', 22, 'Austin, TX', NULL, FALSE, 5125609246, 'ItsHappeningAgain@fakemail.com'),
+    ('Tara', NULL, 'Dactyl', 42, 'Hinesville, GA', 'Ft. Stewart', TRUE, 9127671432, 'Triceratops@fakemail.com'),
+    ('Iva', 'Anita', 'Takashita', 27, 'Fort Riley, KS', 'Ft. Riley', TRUE, 7858913456, 'GottaGo@fakemail.com');
 
 INSERT INTO cohorts (courseid) VALUES
     ('MCSP-19'),
@@ -36,10 +41,25 @@ UPDATE instructors SET cohort_id = 2 WHERE id = 2;
 
 UPDATE students SET branch_id = 1 WHERE id = 1;
 UPDATE students SET branch_id = 3 WHERE id = 2;
+UPDATE students SET branch_id = 2 WHERE id = 3;
+UPDATE students SET branch_id = 3 WHERE id = 4;
+UPDATE students SET branch_id = 4 WHERE id = 5;
+UPDATE students SET branch_id = 1 WHERE id = 6;
+UPDATE students SET branch_id = 2 WHERE id = 7;
 UPDATE students SET instructor_id = 1 WHERE id = 1;
 UPDATE students SET instructor_id = 2 WHERE id = 2;
+UPDATE students SET instructor_id = 1 WHERE id = 3;
+UPDATE students SET instructor_id = 2 WHERE id = 4;
+UPDATE students SET instructor_id = 1 WHERE id = 5;
+UPDATE students SET instructor_id = 2 WHERE id = 6;
+UPDATE students SET instructor_id = 1 WHERE id = 7;
 UPDATE students SET cohort_id = 1 WHERE id = 1;
 UPDATE students SET cohort_id = 2 WHERE id = 2;
+UPDATE students SET cohort_id = 1 WHERE id = 3;
+UPDATE students SET cohort_id = 2 WHERE id = 4;
+UPDATE students SET cohort_id = 1 WHERE id = 5;
+UPDATE students SET cohort_id = 2 WHERE id = 6;
+UPDATE students SET cohort_id = 1 WHERE id = 7;
 
 UPDATE cohorts SET instructor_id = 1 WHERE id = 1;
 UPDATE cohorts SET instructor_id = 2 WHERE id = 2;
