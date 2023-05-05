0a72e5ee33cfdf23e75501ce518789a0eb342035
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-05-05 15:10:43 -0500

    Updated login modal, prepping for page visit effect

--- client/src/context/LoginContext.jsx
+++ client/src/context/LoginContext.jsx
@@ -1,21 +1,27 @@
 import { useState, useContext, createContext } from "react";
 
+
 const LoginContext = createContext();
 
 export const LoginProvider = ({ children }) => {
     const [showLogin, setShowLogin] = useState(false);
     const [loginPosition, setLoginPosition] = useState({ x: 0, y: 0 });
     const [email, setEmail] = useState("");
-    cosnt [password, setPassword] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState(null);
     const [isRegistering, setIsRegistering] = useState(false);
 
     const handleMouseClick = (e) => {
-        const x = e.clientx;
-        const y = e.clienty;
+        const x = e.clientX;
+        const y = e.clientY;
         setLoginPosition({ x, y });
         setShowLogin(true);
     };
+
+    const handleLogin = async () => {
+        // call the API here using the `email` and `password` values
+        // use `setError` to handle any errors
+    }
 
     return (
         <LoginContext.Provider
@@ -27,8 +33,10 @@ export const LoginProvider = ({ children }) => {
                 isRegistering,
                 setIsRegistering,
                 showLogin,
+                setShowLogin,
                 loginPosition,
                 handleMouseClick,
+                handleLogin,
                 error,
                 setError,
             }}
@@ -38,10 +46,10 @@ export const LoginProvider = ({ children }) => {
     );
 };
 
-const useLogin = () => {
+export const useLogin = () => {
     const context = useContext(LoginContext);
     if (!context) {
-        throw new Error("User must login")
+        throw new Error("User must login to see cohorts")
     }
     return context;
 }
