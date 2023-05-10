702baf952f74d0128a6adae0d03defef4133f51c
alphie1028 <alphie1028@gmail.com>
2023-05-10 16:59:19 -0400

    added more testing for left column

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
+    cy.get('.leftColumnArrow').first().click()
+    cy.get('.leftColumnNames').should('be.visible')
+    cy.get('.leftColumnArrow').first().click()
+    cy.get('.leftColumnNames').should('not.exist')
   })
 })
