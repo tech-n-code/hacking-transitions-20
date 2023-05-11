5b8b31a8a4b887e0650179ecccc06355ae5096cb
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-05-11 13:09:52 -0500

    Added register and login buttons and form fields, route not working

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -6,6 +6,9 @@ import Header from "../Header/Header.jsx";
 import CohortDetails from "../CohortRender/CohortDetails.jsx";
 import LeftColumnContext from "../../context/LeftColumnContext";
 import RightColumn from "../RightColumn/RightColumn.jsx";
+import Register from "../Register.jsx";
+import Login from "../Login.jsx";
+import { AuthProvider } from "react-auth-kit";
 
 const App = () => {
 
@@ -13,12 +16,16 @@ const App = () => {
 
   return (
     <>
+      <AuthProvider authStorageType="cookie" authStorageName="_auth_t" authTimeStorageName="_auth_time" stateStorageName="_auth_state" cookieDomain={window.location.hostname} cookieSecure={window.location.protocol==="https:"}>
       <Header />
       <LeftColumn />
+      <Register />
+      <Login />
       {/* <StudentDetail /> */}
       {cohortClicked !== "" ? <CohortDetails />  : <></>}
       {cohortClicked !== "" ? <RightColumn />  : <></>}
       <Footer />
+      </AuthProvider>
     </>
   );
 };
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useSignIn } from "react-auth-kit";
+
+function Login() {
+    const signIn = useSignIn();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("/login", { email, password });
+            // Insert JWT (res.data.token)
+            if(!signIn({token: res.data.token, expiresIn: 7200, remember: true})) {
+                // handle error here, sign in failed
+              }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <form onSubmit={login}>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
