9d387bc5997b5208960f5e0a43f9d44fd385c8d2
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-05-12 17:16:04 -0500

    Updated deployed server file to fix deployed version

--- api/server.js
+++ api/server.js
@@ -3,7 +3,9 @@ import pg from "pg";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
-const db = new pg.Pool({ connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false} });
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 const app = express();
 
@@ -11,13 +13,25 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/cohorts", async (req, res, next) => {
-  const result = await db.query("SELECT * FROM cohorts").catch(next);
+  const result = await db.query("SELECT cohorts.*, instructors.lastname AS instructor_id FROM cohorts INNER JOIN instructors ON cohorts.instructor_id = instructors.id").catch(next);
   res.send(result.rows);
 });
 
 app.get("/api/cohorts/:id", async (req, res, next) => {
   const result = await db
     .query("SELECT * FROM cohorts WHERE id = $1", [req.params.id])
+    .catch(next);
+
+  if (result.rows.length === 0) {
+    res.sendStatus(404);
+  } else {
+    res.send(result.rows);
+  }
+});
+
+app.get("/api/branches", async (req, res, next) => {
+  const result = await db
+    .query("SELECT * FROM branch")
     .catch(next);
 
   if (result.rows.length === 0) {
@@ -38,6 +52,85 @@ app.get("/api/cohorts/:cohortId/students", async (req, res, next) => {
     res.send(result.rows);
   }
 });
+
+app.get("/api/cohorts/:cohortId/students/:studentId", async (req, res, next) => {
+  const cohortId = req.params.cohortId;
+  const studentId = req.params.studentId;
+  const result = await db
+    .query(`SELECT students.* FROM students WHERE students.cohort_id = $1 AND students.id = $2`, [cohortId, studentId])
+    .catch(next);
+  if (result.rows.length === 0) {
+    res.sendStatus(404);
+  } else {
+    res.send(result.rows);
+  }
+});
+
+// Route to get all tasks
+app.get("/api/appointments", async (req, res, next) =>{
+  // const tasks = req.params.tasks
+  const result = await db
+    .query(`SELECT * FROM appointments`)
+    .catch(next)
+  if(result.rows.length === 0){
+    res.sendStatus(404);
+  } else{
+    res.send(result.rows)
+  }
+})
+
+// Route to handle user registration
+app.post("/api/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10); // hashes the password with bcrypt and add 10 extra bits "salt"
+    console.log(req.body);
+
+    console.log("Attempting to insert:", email, hashedPassword);
+    const result = await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, hashedPassword]);
+    console.log('Query result:', result);
+    // JSON Web tokens, yay
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '2h' });
+    res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Registration failed" });
+  }
+});
+
+// Route to handle user login
+app.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [email]);
+    const user = result.rows[0];
+    if (!user) {
+    return res.status(400).json({ error: "Invalide email or password" });
+    }
+    console.log(user);
+    // Compare password to the hashed store
+    const validPassword = await bcrypt.compare(password, user.password);
+    console.log(password, user.password)
+
+    if (!validPassword) {
+      return res.status(400).json({ error: "Invalide email or password" });
+    }
+    // Create JWT
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '2h' });
+
+    res.status(200).json({ token });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Login failed" });
+  }
+});
+
+// Simple route to check users
+app.get("/api/users", async (req, res, next) => {
+  const result = await db.query("SELECT * FROM users").catch(next);
+  res.send(result.rows);
+})
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
