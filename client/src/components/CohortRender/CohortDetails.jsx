3b9f90a2ae4059d152bf8f56ec7f597314cc8f48
alphie1028 <alphie1028@gmail.com>
2023-05-14 08:11:30 -0400

    added ets dates to students

--- client/src/components/CohortRender/CohortDetails.jsx
+++ client/src/components/CohortRender/CohortDetails.jsx
@@ -5,30 +5,10 @@ import '../../styles/CohortDetails.css';
 
 const CohortDetails = () => {
 
-    const { cohortClicked, cohortIdForInfo, cohorts, setCohortClicked} = useContext(LeftColumnContext);
-
+    const { cohortClicked, cohortIdForInfo, cohorts, setCohortClicked, students} = useContext(LeftColumnContext);
+    /* branc_id, dutystatus, phonenumber, ets_date */
     return (
         <>
-           {/* <div className='cohortInfoContainer'>
-                <div className="cohortInfoHeader">
-                    <div className="cohortInfoName">
-                        {cohortClicked}
-                        <div className="cohortClose" onClick={() =>{setCohortClicked("")}}>X</div>
-                    </div>
-                    <div className="cohortInfoTypeContainer">
-                        <div className="cohortInfoType">Start Date</div>
-                        <div className="cohortInfoType">End Date</div>
-                        <div className="cohortInfoType">Instructor</div>
-                        <div className="cohortInfoType">Student Count</div>
-                    </div>
-                </div>
-                <div className="cohortInfoBox">
-                    <div className="cohortInfoEntry">{cohorts[cohortIdForInfo].startdate}</div>
-                    <div className="cohortInfoEntry">{cohorts[cohortIdForInfo].enddate}</div>
-                    <div className="cohortInfoEntry">{cohorts[cohortIdForInfo].instructor_id}</div>
-                    <div className="cohortInfoEntry">{cohorts[cohortIdForInfo].numberofstudents}</div>
-                </div>
-            </div> */}
             <div className="cohortInfoContainer">
                 <div className="cohortInfoHeader">
                     <div className="cohortInfoName">
@@ -42,6 +22,16 @@ const CohortDetails = () => {
                     <div className="CInfo">{`Instructor: ${cohorts[cohortIdForInfo].instructor_id}`}</div>
                     <div className="CInfo">{`Number Of Students: ${cohorts[cohortIdForInfo].numberofstudents}`}</div>
                 </div>
+                {students.map((student, index) => {
+                    return (
+                        <div key={index} className="cohortStudent">
+                            <div>
+                                {student.firstname}
+                                {student.lastname}
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </>
     );
