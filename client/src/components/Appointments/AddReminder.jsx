7083b8d3a9a58aeb853af59a6058e17faec9ace5
iamblake <brbarkmanb@gmail.com>
2023-06-14 11:53:00 -0500

    add/delete initial

--- client/src/components/Appointments/AddReminder.jsx
+++ client/src/components/Appointments/AddReminder.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useContext } from "react";
 import "./Appointments.css";
 import AppointmentContext from "../../context/AppointmentContext";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 
-export default function AddReminder({ setShowAddModal }){
+export default function AddReminder(){
     const { students, setUpdate } = useContext(AppointmentContext);
+    const[ showAddModal, setShowAddModal ] = useState(false)
+    // const {} = useContext(Appointments);
     const[ note, setNote ] = useState("");
     const [ selectedStudent, setSelectedStudent ] = useState("")
 
@@ -24,49 +28,61 @@ export default function AddReminder({ setShowAddModal }){
         })
         
     }
+
+    function closeModal(){
+    setIsStudentModalOpen(false)
+    }
+    
+
     
     return(
-        <span className="Form">
-            <form onSubmit={ handleSubmit }>
-                <div>
-                <label >Add note</label>
-                </div>
-                <textarea 
-                    required
-                    value={ note }
-                    onChange={(e) => setNote(e.target.value)}
-                    rows="5" cols="40"
-                    ></textarea>
-                <div>
-                <select
-                    value={ selectedStudent }
-                    onChange={(e) => setSelectedStudent(e.target.value)}
-                    required
-                >
-                    <option value="" > Please select a student</option>
-                    {students.map((student, x) => {
-                        return (
-                            <option 
-                                key={x} 
-                                value={student.id}
-                            >
-                                {student.firstname} {student.lastname}
-                            </option>
-                        )
-                    })}
-                </select>
-                </div>
-                <div>
-                    <span>
-                <button className="addSubmit">Submit</button>
-                    </span>
-                    <span>
-                <button className="addCancel" onClick={() => setShowAddModal(false)}>
-                    Cancel
-                </button>
-                    </span>
-                </div>
-            </form>
-        </span>
+        <Modal 
+        isOpen={setShowAddModal}
+        onRequestClose={closeModal}
+        style={modalStyle}
+>
+            <span className="Form">
+                <form onSubmit={ handleSubmit }>
+                    <div>
+                    <label >Add note</label>
+                    </div>
+                    <textarea 
+                        required
+                        value={ note }
+                        onChange={(e) => setNote(e.target.value)}
+                        rows="5" cols="40"
+                        ></textarea>
+                    <div>
+                    <select
+                        value={ selectedStudent }
+                        onChange={(e) => setSelectedStudent(e.target.value)}
+                        required
+                    >
+                        <option value="" > Please select a student</option>
+                        {students.map((student, x) => {
+                            return (
+                                <option 
+                                    key={x} 
+                                    value={student.id}
+                                >
+                                    {student.firstname} {student.lastname}
+                                </option>
+                            )
+                        })}
+                    </select>
+                    </div>
+                    <div>
+                        <span>
+                    <button className="addSubmit">Submit</button>
+                        </span>
+                        <span>
+                    <button className="addCancel" onClick={() => setShowAddModal(false)}>
+                        Cancel
+                    </button>
+                        </span>
+                    </div>
+                </form>
+            </span>
+            </Modal>
     )
 }
