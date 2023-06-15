6005efa1a6d378882da908d08071a08e489cd757
Spirit1269 <kimberlydjenkins1@gmail.com>
2023-06-15 12:54:49 -0500

    Updated Header and Footer

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from "react";
-import './Header.css'
+import './Header.css';
 import { useUser } from "../UserProvider.jsx";
+
+
 
 export default function Header(){
     const [ email, setEmail ] = useState("");
     const user = useUser(); 
     const userLoggedIn = user && user.user ? `${user.user.firstname} ${user.user.lastname}` : null;
-
+    ;
     useEffect(() => {
         // console.log(userLoggedIn);
       }, [user]);
-    
      
-
-
-
-    
     // useEffect(() => {
     //     fetch(`http://localhost:3000/api/users`)
     //         .then(response => response.json())
@@ -27,7 +24,7 @@ export default function Header(){
         <span id="header">
             <span id="home-container">
                 <a href="https://www.galvanize.com/">
-                    <img src="https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background.png" alt="Galvanize Logo" />
+                    <img width="259" height="50" src="https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background.png" class="skip-lazy logo-desktop" alt="Galvanize homepage" decoding="async" aria-label="Galvanize homepage" srcset="https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background.png 315w, https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background-232x45.png 232w, https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background-150x29.png 150w, https://www.galvanize.com/wp-content/uploads/2022/11/galvanize_logo_small-scale_full-color_dark-background-250x48.png 250w" sizes="(max-width: 259px) 100vw, 259px"></img>
                 </a>
             </span>
             <span id="title-container">
