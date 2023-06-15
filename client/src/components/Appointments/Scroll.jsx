909b5905b4e2714ab2fffe5bea7614f6363b9099
josh-benton <120067603+josh-benton@users.noreply.github.com>
2023-06-15 18:46:26 -0400

    Updated styling of appointment notes and navbar

--- client/src/components/Appointments/Scroll.jsx
+++ client/src/components/Appointments/Scroll.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) =>{
     return(
-        <div style={{overflowY: 'scroll', height: '275px'}}>
+        <div style={{overflowY: 'scroll', height: '448px'}}>
             {props.children}    
         </div>
     )
