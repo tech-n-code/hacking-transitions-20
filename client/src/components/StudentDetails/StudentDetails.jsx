b35ea596093bf0772de390f8e47f590470954cf9
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-15 19:28:27 -0700

    removed useState import; not used

--- client/src/components/StudentDetails/StudentDetails.jsx
+++ client/src/components/StudentDetails/StudentDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import Modal from "react-modal";
 import "./StudentDetails.css";
 import CohortContext from "../../context/CohortContext";
