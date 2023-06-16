dc0844d04d534e3db99c747f4228cff62897af65
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-15 19:22:13 -0700

    removed useEffect from import; not used

--- client/src/components/CohortDetails/CohortDetails.jsx
+++ client/src/components/CohortDetails/CohortDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import CohortContext from "../../context/CohortContext";
 import "./CohortDetails.css";
 import { useState } from "react";
