ff42585f89e6f5ed63a9f688f084d7fb80e385e7
MDrevon <MsDrevon@gmail.com>
2023-06-21 13:39:40 -0500

    Replaced Calendar component with NewCalendar Component.  Uses FullCalendar library.  NPM install from client directory to add dependencies.  Monthly, Weekly, Single Day and List views implemented.

---
+++ client/src/components/Calendar/NewCalendar.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+
+const NewCalendar = () => {
+  const [events, setEvents] = useState([]);
+  const handleViewChange = (view) => {
+    // Handle view change here if needed
+    console.log("Selected view:", view);
+  };
+
+  useEffect(() => {
+    fetch("/api/calendar")
+      .then((res) => res.json())
+      .then((data) => {
+        const formattedEvents = data.map((event) => ({
+          date: new Date(event.date),
+          title: `${event.title}`,
+        }));
+        setEvents(formattedEvents);
+      })
+      .catch((err) => {
+        console.error("Error fetching Calendar events: ", err);
+      });
+  }, []);
+
+  const headerToolbar = {
+    left: "prev,next today",
+    center: "title",
+    right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+  };
+
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        headerToolbar={headerToolbar}
+        views={{
+          week: {
+            type: "timeGridWeek",
+            duration: { weeks: 1 },
+          },
+          day: {
+            type: "timeGridDay",
+            duration: { days: 1 },
+          },
+        }}
+        onView={() => handleViewChange}
+      />
+    </div>
+  );
+};
+
+export default NewCalendar;
