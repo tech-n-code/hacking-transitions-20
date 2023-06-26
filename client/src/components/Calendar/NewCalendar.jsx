7418c00e3adbdce038a7fb9a3fba352b993af411
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-26 04:57:37 -0700

    refactored; load events, working on toolips

--- client/src/components/Calendar/NewCalendar.jsx
+++ client/src/components/Calendar/NewCalendar.jsx
@@ -3,134 +3,59 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
-// import Modal from "react-modal";
 import "./NewCalendar.css";
-import { Tooltip } from 'react-tooltip';
 import AppointmentContext from "../../context/AppointmentContext.jsx";
-
-// Modal.setAppElement("#root");
+import CohortContext from "../../context/CohortContext.jsx";
+import { Tooltip } from 'react-tooltip';
 
 const NewCalendar = () => {
-  // const calendarRef = useRef(null);
-  // const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [isAddEventOpen, setIsAddEventOpen] = useState(false);
   const [calendarEvents, setCalendarEvents] = useState([]);
   const { events } = useContext(AppointmentContext);
+  const { students } = useContext(CohortContext);
 
-  // const openAddEventModal = () => {
-  //   setIsAddEventOpen(true);
-  // };
+  console.log(events);
 
   const handleViewChange = (view) => {
-    // Handle view change here if needed
     console.log("Selected view: " + view);
   };
 
   const handleEventClick = (info) => {
     setSelectedEvent(info.event);
-    // setModalIsOpen(true);
     console.log("Selected event: " + selectedEvent);
   };
 
-  // const handleModalClose = () => {
-  //   setModalIsOpen(false);
-  //   setSelectedEvent(null);
-  // };
+  const getEventOwner = (givenStudentId) => {
+    const eventOwner = students.find((student) =>
+    student.id === givenStudentId);
+    const eventOwnerFirstName = eventOwner ? eventOwner.firstname : null;
+    const eventOwnerLastName = eventOwner ? eventOwner.lastname : null;
+    return eventOwnerFirstName + " " + eventOwnerLastName;
+  }
 
   useEffect(() => {
     const formattedEvents = events.map((event) => ({
-      date: new Date(event.startdate),
-      title: event.title,
+      start: new Date(event.startdate),
+      end: new Date(event.enddate),
+      title: event.title + ": " + getEventOwner(event.student_id),
       allDay: event.allday,
+      extendedProps: {
+        'data-tip': event.title + ": " + getEventOwner(event.student_id)
+      }
     }));
     setCalendarEvents(formattedEvents);
   }, []);
-  
-
-  // useEffect(() => {
-  //   fetch("/api/events")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const formattedEvents = data.map((event) => ({
-  //         date: new Date(event.startdate),
-  //         title: `${event.title}`,
-  //         allDay: `${event.allday}`,
-  //       }));
-  //       setEvents(formattedEvents);
-  //       console.log(formattedEvents)
-  //     })
-  //     .catch((err) => {
-  //       console.error("Error fetching Calendar events: ", err);
-  //     });
-  // }, []);
-
-  // useEffect(() => {
-  //   const calendarApi = calendarRef.current.getApi();
-
-  //   const updateEventRender = () => {
-  //     const eventElements =
-  //       calendarApi.el.getElementsByClassName("fc-event-main");
-
-  //     Array.from(eventElements).forEach((element) => {
-  //       const title = element.innerText;
-
-  //       const tooltip = document.createElement("div");
-  //       tooltip.className = "event-tooltip";
-  //       tooltip.textContent = title;
-
-  //       element.addEventListener("mouseenter", () => {
-  //         element.appendChild(tooltip);
-  //       });
-
-  //       element.addEventListener("mouseleave", () => {
-  //         element.removeChild(tooltip);
-  //       });
-  //     });
-  //   };
-
-  //   calendarApi.on("datesRender", updateEventRender);
-
-  //   return () => {
-  //     calendarApi.off("datesRender", updateEventRender);
-  //   };
-  // }, []);
 
   const headerToolbar = {
     left: "prev,next today",
     center: "title",
     right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
-    // extraButtons: [
-    //   {
-    //     text: "Add Event",
-    //     click: openAddEventModal,
-    //   },
-    // ],
   };
-
-  // const modalStyle = {
-  //   content: {
-  //     position: "absolute",
-  //     top: "50%",
-  //     bottom: "50%",
-  //     left: "50%",
-  //     right: "50%",
-  //     transform: "translate(-50%, -50%)",
-  //     height: "fit-content",
-  //     width: "fit-content",
-  //     border: "1px solid #ccc",
-  //     background: "#fff",
-  //     overflow: "auto",
-  //     borderRadius: "10px",
-  //     outline: "none",
-  //     padding: "30px",
-  //   },
-  // };
 
   return (
     <div>
       <FullCalendar
-        // ref={calendarRef}
         plugins={[dayGridPlugin, timeGridPlugin, listPlugin]}
         initialView="dayGridMonth"
         events={calendarEvents}
@@ -148,18 +73,7 @@ const NewCalendar = () => {
         }}
         onView={() => handleViewChange}
       />
-      {/* <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={handleModalClose}
-        contentLabel="Event Details"
-        style={modalStyle}
-      >
-        <div>
-          <h2>{selectedEvent?.title}</h2>
-          <p>{selectedEvent?.startdate?.toDateString()}</p>
-          <button onClick={handleModalClose}>Close</button>
-        </div>
-      </Modal> */}
+      <Tooltip effect="solid" />
     </div>
   );
 };
