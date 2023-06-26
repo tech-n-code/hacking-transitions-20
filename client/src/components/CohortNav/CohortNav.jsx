5375cac0811967d8c549e4fdaa53029d7e446c16
Will Franceschini <113144533+tech-n-code@users.noreply.github.com>
2023-06-26 03:31:09 -0700

    refactored to integrate CohortNavDrops

--- client/src/components/CohortNav/CohortNav.jsx
+++ client/src/components/CohortNav/CohortNav.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import CohortNavDrops from "./CohortNavDrops";
+import React, { useContext } from "react";
 import "./CohortNav.css";
+import CohortContext from "../../context/CohortContext";
 
 function CohortNav() {
+  const {
+		handleCohortClicked,
+		cohorts,
+	} = useContext(CohortContext);
+  
   return (
     <div className="cohortNav">
       <div className="cohortNavHeader">Cohorts</div>
       <div className="cohortNavDrop-container">
-        <CohortNavDrops />
+        <>
+          {cohorts.map((cohort, index) => {
+            return (
+              <React.Fragment key={index}>
+                <div className="cohortNavDrop" onClick = {() => handleCohortClicked(cohort.title, cohort.id)}>
+                  {cohort.title}
+                </div>
+              </React.Fragment >
+            );
+          })}
+        </>
       </div>
     </div>
   );
